4a6c0ba1bb4e9b4bf1cee6e9da9cdc7c9e935598
sharjeel123456789 <m.sharjeel1244@gmail.com>
2024-04-15 22:38:01 +0500

    cli guess game

---
+++ index.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuuessedNumber",
+        type: "Number",
+        message: "Please guess the Number between 1 to 6: ",
+    },
+]);
+if (answer.userGuuessedNumber === randomNumber) {
+    console.log("congratulation you guess rightnumber.");
+}
+else {
+    console.log("you guess wrong number");
+}
